573a7cd5006261e70f66c739035bd83fb26335b6
Isaiah Alcayde <isaiahalcayde@gmail.com>
2026-04-19 00:25:36 -0700

    Main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+  
+
+int main() {
+    int e;
+    int n;
+
+    cout << "Enter e" << endl;
+    cin >> e;
+    cout << "Enter n" << endl;
+    cin >> n;
+
+    cout << e + n << endl;
+    return 0;
+}
