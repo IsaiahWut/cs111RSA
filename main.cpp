b2106662f77a871718d8ffe912329dd91c1c865e
Isaiah Alcayde <isaiahalcayde@gmail.com>
2026-04-19 20:04:14 -0700

    Updated code

--- main.cpp
+++ main.cpp
@@ -3,9 +3,14 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <cmath>
 
 using namespace std;
   
+// gcd function check
+int gcd(int a, int b) {
+    
+}
 
 int main() {
     int e;
@@ -49,20 +54,58 @@ int main() {
     alphabet[37] = '\''; // apostrophe
     
     
-
-    cout << "Enter e" << endl;
+    cout << "Enter e" << endl; //
     cin >> e;
-    cout << "Enter n" << endl;
+    cout << "Enter n" << endl; //
     cin >> n;
 
     cout << e << ", " << n << endl;
+    int p = 0;
+    int q = 0;
 
-    // find p and q 
+    string decodedMessageIntegers;
+    string decodedMessageCharacters;
+
+    // find p and q - only find up to sqrt(n)
+    int sqrt_n = sqrt(n);
+
+    // loop from i to sqrt(n) to find the prime numbers of n
+    for(int i = 3; i <= sqrt_n; i++){
+        if(n % i == 0){ // if n is divisible by i, then that means we have found one prime
+            p = i;
+            break;
+        }
+    }
+    // to find q, we can divide n/p to find q
+    q = n/p;
 
     // find phi(n)
+    int phi = (p - 1) * (q - 1);
+    
+    bool dFlag = false;
     // check if gcd(e, phi(n) == 1)
+    int gcdCheck = gcd(e, phi);
+    
+    if(gcdCheck == 1){
+        dFlag = true;
+    }
+    else{
+        cout << "GCD is not found." << endl;
+    }
+
+
+    int d = 0;
     // while loop to take in each crypted letter and decrypt
+    // compute decryption exponent d = e^{-1}(mod phi)) 
+    
+
 
     // output final decrypted message
+    // decrpy the message = M = C^d rem n.
+
+
+    cout << p << ", " << q << ", " << phi << d << endl;
+    cout << decodedMessageIntegers << endl;
+    cout << decodedMessageCharacters << endl;
     return 0;
 }
